02c6e0fd66770c4fccb7f9b497432bbfbc330148
Ruben de Vries <rubensayshi@gmail.com>
2011-08-13 19:14:49 +0200

    cleanup

--- app/autoload.php
+++ app/autoload.php
@@ -14,6 +14,10 @@ $loader->registerNamespaces(array(
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    'GoGreat'          => array(
+							__DIR__.'/../src',
+							__DIR__.'/../vendor/symfony-target-cnfs/src',
+						  ),
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
--- web/app.php
+++ web/app.php
@@ -5,8 +5,9 @@ require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use GoGreat\SymfonyWrapper\SymfonyApp;
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel(SymfonyApp::getMode(), SymfonyApp::getDebugMode());
+
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();
